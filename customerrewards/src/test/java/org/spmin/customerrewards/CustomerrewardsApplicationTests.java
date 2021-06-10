a4166f2feb556c1efddc5558a5b43ca0a505ad79
Shaun Minnihan <spminnihan@gmail.com>
2021-06-10 11:46:23 -0500

    initial commit

---
+++ customerrewards/src/test/java/org/spmin/customerrewards/CustomerrewardsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spmin.customerrewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerrewardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
