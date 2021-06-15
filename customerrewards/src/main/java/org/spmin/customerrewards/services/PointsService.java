1c652e672c9602707b0816770f6b63081a99e348
Shaun Minnihan <spminnihan@gmail.com>
2021-06-14 20:21:57 -0500

    refactored points service to generate points for each transaction individually then sum them for the customer

--- customerrewards/src/main/java/org/spmin/customerrewards/services/PointsService.java
+++ customerrewards/src/main/java/org/spmin/customerrewards/services/PointsService.java
@@ -7,25 +7,36 @@ import java.util.List;
 
 public class PointsService {
 
-    // calculates a customer's rewards points
+
+    // calculate points for an individual transaction and save to that transaction
     // Every dollar spent over $100 is worth 2 points
     // and every dollar spent over $50 is worth 1 point
-    static public int calculatePoints(Customer customer) {
+    static public int calculateTransactionPoints(double transactionAmt) {
+
+        int transactionPoints = 0;
+
+       if (transactionAmt > 100) {
+           transactionPoints += 50 + 2 * (Math.floor(transactionAmt) - 100);
+        } else if (transactionAmt > 50) {
+           transactionPoints += Math.floor(transactionAmt) - 50;
+        }
+       //else do nothing
+
+       return transactionPoints;
+    }
+
+    // calculates a customer's rewards points and sums them
+    static public void calculateCustomerPoints(Customer customer) {
 
         int pointsTotal = 0;
         List<Transaction> transactionList = customer.getTransactions();
 
-        for (Transaction transaction : transactionList) {
-            double transactionAmt = Math.floor(transaction.getTransactionAmount());
-
-            if (transactionAmt > 100) {
-                pointsTotal += 50 + 2 * (transactionAmt - 100);
-            } else if (transactionAmt > 50) {
-                pointsTotal += transactionAmt - 50;
-            }
-            // else do nothing
+        for(Transaction transaction : transactionList) {
+            int transactionPoints = PointsService.calculateTransactionPoints(transaction.getTransactionAmount());
+            transaction.setTransactionPoints(transactionPoints);
+            pointsTotal += transactionPoints;
         }
+        customer.setRewardsPoints(pointsTotal);
 
-        return pointsTotal;
     }
 }
