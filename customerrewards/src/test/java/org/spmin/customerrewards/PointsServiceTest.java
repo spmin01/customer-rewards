7ac8d602899630b5f4e4dbbab973c39c710c4c23
Shaun Minnihan <spminnihan@gmail.com>
2021-06-14 21:02:32 -0500

    customer points sum test passes

--- customerrewards/src/test/java/org/spmin/customerrewards/PointsServiceTest.java
+++ customerrewards/src/test/java/org/spmin/customerrewards/PointsServiceTest.java
@@ -1,13 +1,33 @@
 package org.spmin.customerrewards;
 
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.spmin.customerrewards.models.Customer;
+import org.spmin.customerrewards.models.Transaction;
 import org.spmin.customerrewards.services.PointsService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class PointsServiceTest {
 
+    private static Customer customer;
+
+    @BeforeAll
+    public static void createCustomer() {
+        customer = new Customer();
+        List<Transaction> transactionList = new ArrayList<>();
+
+        for(int i = 0; i < 5; i++) {
+            Transaction transaction = new Transaction(30 *  (i + 1)); // eg 30, 60, 90, 120, 150
+            transactionList.add(transaction);
+        }
+
+        customer.setTransactions(transactionList);
+    }
 
     // Case 1: transaction amount exactly $50, expected value 0
     @Test
@@ -33,5 +53,14 @@ public class PointsServiceTest {
     public void pointsCalcOverHundred() {
         assertEquals(100, PointsService.calculateTransactionPoints(125.0));
         assertEquals(150, PointsService.calculateTransactionPoints(150.0));
+    }
+
+    // test customer points summation
+    // test case transaction values 30, 60, 90, 120, 150, expected total: 290
+    @Test
+    public void customerPointsCalc() {
+
+        PointsService.calculateCustomerPoints(customer);
+        assertEquals(290, customer.getRewardsPoints());
     }
 }
